2ef1223925711a7001cde19bbbfa59619a182fac
Panda291 <christophevandeneede@hotmail.com>
2019-12-14 15:04:36 +0100

    added a class for unit tests and implemented unit tests

---
+++ TestProject1/Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using XamarinProject.Models;
+using XamarinProject.Services;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void falseForLowerThan5()
+        {
+            ClassForUnitTest obj = new ClassForUnitTest();
+            var output = obj.isBetween5and10(4);
+            Assert.AreEqual(output, -1, 0, "input smaller than 5 but output not -1");
+        }
+
+        [Test]
+        public void falseForHigherThan10()
+        {
+            ClassForUnitTest obj = new ClassForUnitTest();
+            var output = obj.isBetween5and10(11);
+            Assert.AreEqual(output, 1, 0, "input higher than 10 but output not 1");
+        }
+
+        [Test]
+        public void trueForEquals5()
+        {
+            ClassForUnitTest obj = new ClassForUnitTest();
+            var output = obj.isBetween5and10(5);
+            Assert.AreEqual(output, 0, 0, "input is 5 but output not 0");
+        }
+
+        [Test]
+        public void trueForEquals10()
+        {
+            ClassForUnitTest obj = new ClassForUnitTest();
+            var output = obj.isBetween5and10(10);
+            Assert.AreEqual(output, 0, 0, "input is 10 but output not 0");
+        }
+
+        [Test]
+        public void trueForbetween5and10()
+        {
+            ClassForUnitTest obj = new ClassForUnitTest();
+            var output = obj.isBetween5and10(7);
+            Assert.AreEqual(output, 0, 0, "input is valid but output not 0");
+        }
+    }
+}
